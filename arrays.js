4c56bee217617b252fb4443a49fd8dda8fbcb340
YgorMiudo <araujo.ygor@escola.pr.gov.br>
2026-04-23 14:11:46 +0000

    trabalho 4 realizado

---
+++ arrays.js
@@ -0,0 +1,51 @@
+const edFolha = [
+  {
+  titulo: "Overwatch",
+  preco: 215
+  },
+  {
+    titulo: "Engenharia Mecanica",
+    preco: 125
+  },
+  {
+    titulo: "Diario de um banana",
+    preco: 430
+  },
+  {
+    titulo: "Carros",
+    preco: 545
+  },
+  {
+    titulo: "Elixir",
+    preco: 750
+  }
+]
+
+const edGalho = [
+  {
+    titulo: "Pokemon",
+    preco: 920
+  },
+  {
+    titulo: "Invencible",
+    preco: 622
+  },
+  {
+    titulo: "Hallo",
+    preco: 228
+  },
+  {
+    titulo: "Grande sertão",
+    preco: 339
+  },
+  {
+    titulo: "Friends",
+    preco: 351
+  },
+  {
+    titulo: "While",
+    preco: 403
+  }
+]
+
+module.exports = { edFolha, edGalho }
